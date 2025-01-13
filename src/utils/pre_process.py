4d480b313aba1b2f5b3c4fea490ec8bcd9255393
vik-vik-ironblocks <131665855+vik-vik-ironblocks@users.noreply.github.com>
2025-01-13 16:31:08 +0200

    chore: refactor lib structure

---
+++ src/utils/pre_process.py
@@ -0,0 +1,27 @@
+import pickle, os
+
+directory = 'src'
+
+print (__file__, "file")
+
+def clean_tokens(tokens):
+    clean_tokens = []
+
+    for token in tokens:
+        if token.startswith('0x') or token == '' or token == ' ' or ('Unknown' in token):
+            continue
+        clean_tokens.append(token)
+    return clean_tokens
+
+
+def pre_process_opcode(opcode, training=False):
+
+    if training:
+        with open(os.path.join(directory,'model_training','vectorizer.pkl'), 'rb') as f:
+            loaded_vectorizer = pickle.load(f)
+    else:
+        with open(os.path.join(directory, 'working_model','vectorizer.pkl'), 'rb') as f:
+            loaded_vectorizer = pickle.load(f)
+    tokens = clean_tokens(opcode)
+    return(loaded_vectorizer.transform(tokens))
+
